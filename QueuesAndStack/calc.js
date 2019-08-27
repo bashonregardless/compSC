91ab524351db42cda0a9067be9805af7d0d3a5ed
harsh <harshvardhan.sharma@innovaccer.com>
2019-08-27 20:25:18 +0530

    calculator implementation 1

--- QueuesAndStack/calc.js
+++ QueuesAndStack/calc.js
@@ -14,13 +14,14 @@ const stdIn = require('./input'),
   Calc = Object.create(stdIn);
 
 /* constructor function to generate token object */
-function Token(value) {
+function Token(type, value) {
   this.token_value = value;
+  this.token_type = type;
 }
 
 Calc.init = function setup() {
   this.expr = null;
-  this.queue_infix;
+  this.queue_infix = [];
   this.queue_postfix = [];
   this.input();
 
@@ -33,51 +34,68 @@ Calc.init = function setup() {
 	'NEGATE'
   ];
 
+  /* operator to symbol mapping */
+  this.op_to_sym = {
+	'ADD': '+',
+	'SUBTRACT': '-',
+	'MULTIPLY': '*',
+	'DIVIDE': '%',
+	'NEGATE': '-',
+  }
+
   /* number of operands for each operator type */
   this.op_operands = [2, 2, 2, 2, 1];
 
   /* order-of-operations (precedence) (0 = evaluated last) */
   this.op_precendences = [0, 0, 1, 1, 2];
+
+  /* evaluation direction (associativity) for each precedence level */
+  this.op_associativity = ['LEFT', 'LEFT', 'RIGHT'];
 }
 
 Calc.input = async function input() {
   const user_input = await stdIn.requestInput("Insert expresstion (infix): ");
-  this.queue_infix = user_input.split(" ").map(function initializeTokens(token_val) {
-	return new Token(token_val);
-  });
   this.exprToInfix(user_input);
   this.infixToPostfix();
+  this.evaluatePostfix();
 }
 
 Calc.exprToInfix = function expr_to_infix(expression) {
   let type = 'OPERATOR';
+
   for (const [i, ch] of counteredIterables(expression)) {
-	//for (let i = 0; queue_infix[i]; i++) {
-	if (ch) {
-	  if (queue_infix[i].token_value.length === 1) {
-		switch(queue_infix[i].token_value) {
+	if (ch !== " ") {
+	  /* contains value of token */
+	  const token_value = {
+		operand: null, /* numeric value of operand */
+		op_code: null, /* opcode for operators */
+	  };
+
+	  if (ch.length === 1) { /* operators are all 1 character */
+		switch(ch) {
 		  case '+':
-			queue_infix[i].token_type = 'OPERAND';
+			type = 'OPERATOR';
+			token_value.op_code = 'ADD';
 			break;
 		  case '-':
 			/* check previous token to distinguish between
 			 negate (unary) and subtract (binary) */
 			if (type === 'OPERATOR') {
-			  queue_infix[i].op_code = 'NEGATE';
+			  token_value.op_code = 'NEGATE';
 			} else if (type === 'LPARENS') {
-			  queue_infix[i].op_code = 'NEGATE';
+			  token_value.op_code = 'NEGATE';
 			} else {
-			  queue_infix[i].op_code = 'SUBTRACT';
+			  token_value.op_code = 'SUBTRACT';
 			}
 			type = 'OPERATOR';
 			break;
 		  case '*':
 			type = 'OPERATOR';
-			queue_infix[i].op_code = 'MULTIPLY';
+			token_value.op_code = 'MULTIPLY';
 			break;
 		  case '/':
 			type = 'OPERATOR';
-			queue_infix[i].op_code = 'DIVIDE';
+			token_value.op_code = 'DIVIDE';
 			break;
 		  case '(':
 			type = 'LPARENS';
@@ -86,26 +104,83 @@ Calc.exprToInfix = function expr_to_infix(expression) {
 			type = 'RPARENS';
 			break;
 		  default:
+			/* not an operator */
 			type = 'OPERAND';
+			token_value.operand = +ch;
 		}
 	  } else {
 		type = 'OPERAND';
+		token_value.operand = +ch;
 	  }
-	  queue_infix[i].token_type = type;
+	  /* add token with parsed type and value to end of queue */
+	  this.queue_infix.push(new Token(type, token_value));
 	}
   }
 }
 
+/* TO DO: Error Handling 
+ * The program would crash on many invalid inputs */
 Calc.infixToPostfix = function infix_to_postfix() {
+  const op_stack = [];
+  const op_idx = this.op.indexOf;
   this.queue_infix.forEach(function parse_infix(token) {
-	if (token.token_type === 'OPERATOR') {
+	if (token.token_type === 'LPARENS') {
+	  op_stack.push(token.token_type);
+	} else if (token.token_type === 'RPARENS') {
+	  while (op_stack[op_stack.length - 1] !== 'LPARENS') {
+		let popped_tok = op_stack.pop();
+		if (popped_tok !== 'LPARENS') {
+		  this.queue_postfix.push(popped_tok);
+		}
+	  };
+	  op_stack.pop();
 	} else {
-	  this.queue_postfix.push(token.token_value);
+	  if (token.token_type === 'OPERATOR') {
+		while (this.op_precendences[this.op.indexOf(op_stack[op_stack.length - 1])] >=
+		  this.op_precendences[this.op.indexOf(token.token_value.op_code)]) {
+		  if (this.op_precendences[this.op.indexOf(op_stack[op_stack.length - 1])] >
+			this.op_precendences[this.op.indexOf(token.token_value.op_code)]) {
+			this.queue_postfix.push(op_stack.pop());
+		  } else if (this.op_precendences[this.op.indexOf(op_stack[op_stack.length - 1])] === 
+			this.op_precendences[this.op.indexOf(token.token_value.op_code)] && 
+			this.op_associativity[this.op.indexOf(token.token_value.op_code)] === 'LEFT') {
+			this.queue_postfix.push(op_stack.pop());
+		  }
+		}
+		op_stack.push(token);
+	  } else {
+		this.queue_postfix.push(token);
+	  }
 	}
   }.bind(this));
 
+  while (op_stack.length > 0) {
+	this.queue_postfix.push(op_stack.pop());
+  }
+
   console.log(this.queue_infix);
-  console.log(this.queue_postfix);
+  console.log("POSTFIX", this.queue_postfix);
+}
+
+Calc.evaluatePostfix = function evaluate_postfix() {
+  const postfix_stack = [];
+  let ans;
+  this.queue_postfix.forEach(function ev_postfix(token) {
+	if (token.token_type === 'OPERAND') {
+	  postfix_stack.push(token.token_value.operand)
+	} else {
+	  if (this.op_operands[this.op.indexOf(token.token_value.op_code)] === 2) {
+		const operand_two = postfix_stack.pop(),
+		  operand_one = postfix_stack.pop();
+		postfix_stack.push(eval(operand_one + this.op_to_sym[token.token_value.op_code] + operand_two))
+	  }
+	}
+  }.bind(this));
+  
+  if (postfix_stack.length > 0) {
+	ans = postfix_stack.pop();
+	console.log("ans: ", ans);
+  }
 }
 
 Calc.userQueue = function userQueue() {
