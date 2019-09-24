afc464440fca989e46461fcd14aff4b05e5d2598
Harshvardhan Sharma <i0682@IT000241.local>
2019-09-25 00:14:05 +0530

    BTree deletion | merge in development

--- BTree/BTree.js
+++ BTree/BTree.js
@@ -17,12 +17,47 @@ BTree.newNode = function New_Node (DEGREE) {
 BTree.setup = function setup () {
   this.DEGREE = 3;
   this.root = new this.newNode(this.DEGREE);
-  this.inputKeys = [8, 1, 11, 5, 13, 7, 28, 37, 16, 12, 3, 15, 17, 27, 6, 9, 14, 43, 2, 4, 20, 22, 25, 26, 22];
+  /* to check for predecessor (case 2.a), uncomment 27.45, 27.46, 27.47, .
+   * to check for successor (case 2.b), uncomment 27.89, .
+   */
+  this.inputKeys = [8, 1, 11, 5, 13, 7, 28, 37, 16, 12, 3, 15, 17, 27, 27.1, 27.66, 27.12, 27.15, 27.84, 27.44, /*27.45, 27.46, 27.47,*/ 27.53, 27.56, 27.88, /*27.89,*/ 27.23, 27.49, 27.99, 27.06, 27.39, 27.77, 27.61, 27.48, 27.93, 27.96, 27.2, 27.5, 27.3, 27.6, 6, 9, 14, 43, 2, 4, 20, 22, 25, 26];
   //await input.createInput();
   /* GOTCHA: If forEach callback is not bound with the scope of BTree, it references (verify) global object */
   this.inputKeys.forEach(function insertKey(key) { this.insertNode(key) }.bind(this));
+  this.deleteNode(22);
 }
 
+/* BTree search takes as input a pointer to root node (not if available in scope) x
+ * of a subtree and a key k to be searched for in that subtree.
+ * The top level call is thus of the form search_node(T.root, k).
+ * If k is in the BTree, search_node returns the ordered pair (y, i) consisting of 
+ * node y and an index i such that y.key_suffix_i = i. Otherwise, the procedure returns
+ * NIL.
+ */
+BTree.searchNode = function search_node (node, key) {
+  var i = 1;
+
+  /* find the smallest index i such that k <= x.key_suffix_i, or else set x.n to x.n + 1 */
+  while (i <= node.total_keys && key > node.keys[i - 1]) {
+	i++;
+  }
+
+  /* check to see if we have discovered the key, returning if we have */
+  if ( i <= node.total_keys && key === node.keys[i - 1] ) {
+	return [node, i];
+  }
+
+  /* terminate the search unsuccesfully ( if x is a leaf) */
+  else if (node.leaf) {
+	return;
+  }
+
+  /* recurse to search appropriate subtree of x */
+	/* if return keyword is not used, 
+	 * TypeError: this.searchNode is not a function or its return value is not iterable
+	 */
+  else return this.searchNode(node.children[i - 1], key);
+}
 BTree.splitNode = function split_node (node, i) {
   /* create node z (new split node) */
   var new_split_node = new this.newNode(this.DEGREE);
@@ -95,7 +130,7 @@ BTree.splitNode = function split_node (node, i) {
    * while (original_split_node.keys[l] < node.keys[l]) {
    */
   while (l >= 1 && original_split_node.keys[this.DEGREE - 1] < node.keys[l - 1]) {
-	node.keys[l + 1] = node.keys[l];
+	//node.keys[l + 1] = node.keys[l];
 	/* GOTCHA: */
 	/* node.keys[l + 1] = node.keys[l]; */
 	node.keys[l] = node.keys[l - 1];
@@ -135,7 +170,9 @@ BTree.insertNonfull = function insert_nonfull (sp_node, key) {
 
 	  /* GOTCHA: indexing error and comparison operator wrong */
 	  //while (key < sp_node.keys[i]) {
-	  while (key > sp_node.keys[i - 1]) {
+	  /* GOTCHA: no check for comparison to terminate at correct index */
+	  /* while (key > sp_node.keys[i - 1]) { */
+	  while (i <= sp_node.total_keys && key > sp_node.keys[i - 1]) {
 		i = i + 1;
 	  }
 	}
@@ -173,12 +210,86 @@ BTree.mergeNode = function merge_node () {
 
 }
 
-BTree.deleteNode = function delete_node () {
-  var node = this.root;
+/* GOTCHA: called like this
+ * BTree.findPredecessor = function find_predecessor (node, i) { 
+ * This needs adjustment of arg i after first recursive call. 
+ */
+BTree.findPredecessor = function find_predecessor (node) {
+  if (node.total_keys > this.DEGREE - 1 && !node.leaf) {
+	/* i - 1, since we are searching for predecessor */
+	/* GOTCHA: if return keyword is not used, return from recursive call is undefined*/
+	/* GOTCHA: return this.findPredecessor(node.children[node.children[i - 1], i);
+	 * Now arg node.children[i -  1] moved to intital call to find_predecessor in delete_node
+	 */
+	return this.findPredecessor(node.children[node.total_keys]);
+  } else {
+	var predecessor = node.keys[node.total_keys - 1];
+	node.keys[node.total_keys - 1] = null;
+	return predecessor;
+  }
+}
+
+BTree.findSuccessor = function find_successor (node) {
+  if (node.total_keys > this.DEGREE - 1 && !node.leaf) {
+	return this.findSuccessor(node.children[0]);
+  } else {
+	var successor = node.keys[node.total_keys - 1];
+	node.keys[node.total_keys - 1] = null;
+	return successor;
+  }
+}
+
+BTree.deleteNode = function delete_node (key) {
+  var [node, i] = this.searchNode(this.root, 27.53);
 
   /* If node is an internal node (not a leaf) */
+  /* TO-DO: If the node to be deleted is somewhere not at the end, then 
+	* adjust the keys index accordingly */
   if (!node.leaf) {
+
+	/* find the predecessor of k.
+	 * Predecessor is found by recursively searching for the right most key.
+	 */
+	if (node.children[i - 1].total_keys > this.DEGREE - 1) {
+	  /* GOTCHA: Tried to implement findPredecessor like
+	   * var predecessor = this.findPredecessor(node, i);
+	   */
+	  var predecessor = this.findPredecessor(node.children[i - 1]);
+	  node.keys[i - 1] = predecessor;
+	}
 	
+	/* find the successor of k.
+	 * Successor are found by recursively searching for the left most node.
+	 */
+	else if (node.children[i].total_keys > this.DEGREE - 1) {
+	  var successor = this.findSuccessor(node.children[i]);
+	  node.keys[i- 1] = successor;
+	}
+
+	/* merge nodes */
+	else {
+	  /* copy key to be deleled in y */
+	  node.children[i - 1].keys[this.DEGREE - 1] = node.keys[i - 1];
+
+	  /* copy all the keys from z to y */
+	  var j = 0;
+	  while (j < this.DEGREE - 1) {
+		node.children[i - 1].keys[j + this.DEGREE] = node.children[i].keys[j];
+		j++;
+	  }
+
+	  /* copy all children of z to y */
+	  var k = 0;
+	  while (k <= this.DEGREE - 1) {
+		node.children[i - 1].children[k + this.DEGREE] = node.children[i].children[k];
+		k++;
+	  }
+	}
+  } else {
+	/*  (case 2.b) successor key k_prime of k */
+	var succ_key = node.keys[0];
+	/* Convert to adjusted array */
+	node.keys[0] = null;
   }
 }
 
