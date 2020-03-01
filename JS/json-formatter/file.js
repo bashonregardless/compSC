55be72dcb7ba409737cc7bde001307b9b61cd428
bashonregardless <harshvardhan0622@gmail.com>
2020-03-01 13:58:51 +0530

    JSON error handling, This still prings brackets contents that are invaild json when Indent > 0

--- JS/json-formatter/file.js
+++ JS/json-formatter/file.js
@@ -18,16 +18,17 @@ String.prototype.repeat = function(length) {
   return Array(length + 1).join(this);
 };
 
-const Formatter = {},
-  stack = [];
-
-Formatter.parsejson = function parse_json () {
+function formatJson () {
   //const jsonString = `{"data":{"name":"\\nharsh"}}`
   const jsonString = process.argv[2];
 
-  let indent = 0;
+  // token tracking info
   let prevToken = "";
   let stringLiteral = false;
+  
+  // indent % 2 gives line number
+  let indent = 0;
+  let lineNumber = 0;
 
   jsonString.split('').forEach(function parseStr (token) {
 	if (stringLiteral) {
@@ -38,26 +39,44 @@ Formatter.parsejson = function parse_json () {
 	  switch (token) {
 		case '[':
 		case '{':
-		  if (prevToken === '{' || prevToken === '[') {
-			process.stdout.write(`\n${" ".repeat(indent)}${token}`);
-			indent = indent + 2;
-		  }
-		  else {
-			indent = indent + 2;
-			process.stdout.write(`${token}`);
-		  }
-		  break;
-
 		case ']':
 		case '}':
-		  indent = indent - 2;
-		  if (prevToken === '{' || prevToken === '[') {
-			process.stdout.write(`${token}`);
-		  } else
-			process.stdout.write(`\n${" ".repeat(indent)}${token}`);
-		  break;
+		  try {
+			if (token === '[' || token === '{') {
+			  lineNumber += 1;
+			  if (prevToken === '{' || prevToken === '[') {
+				// If indent becomes negative, throw parantheses balance error: Invalid JSON
+				process.stdout.write(`\n${" ".repeat(indent)}${token}`);
+				indent = indent + 2;
+			  }
+			  else {
+				indent = indent + 2;
+				process.stdout.write(`${token}`);
+			  }
+			  break;
+			}
+
+			if (token === ']' || token === '}') {
+			  lineNumber += 1;
+			  indent = indent - 2;
+
+			  if (prevToken === '{' || prevToken === '[') {
+				process.stdout.write(`${token}`);
+			  } else
+				process.stdout.write(`\n${" ".repeat(indent)}${token}`);
+			  break;
+			}
+		  }
+		  catch (err) {
+			console.log(`\n\n**** ERROR *****\nLast matched line : ${lineNumber}\nLook for error till last matching collection type\n\n**********\n\n`);
+			throw new Error('Check JSON');
+		  }
 
 		case ',':
+		  if (!(prevToken === '}' || prevToken === ']')) {
+			lineNumber += 1;
+		  }
+
 		  process.stdout.write(`${token}\n${" ".repeat(indent)}`);
 		  break;
 
@@ -68,7 +87,7 @@ Formatter.parsejson = function parse_json () {
 		case '"':
 		  if ( !stringLiteral && prevToken === ':')
 			stringLiteral = true;
-		  
+
 		default:
 		  if (prevToken === '{' || prevToken === '[')
 			process.stdout.write(`\n${" ".repeat(indent)}${token}`);
@@ -81,7 +100,13 @@ Formatter.parsejson = function parse_json () {
   }.bind(this));
 
   process.stdout.write("\n");
+
+  if (indent > 0) {
+	// print in reverrse order till last collection type ( '{' || '[') token
+	console.log(`\n\n**** ERROR *****\nLast matched line : ${lineNumber}\nLook for error till last matching collection type\n\n**********\n\n`);
+	throw new Error('Check JSON');
+  }
   process.exit();
 }
 
-Formatter.parsejson();
+formatJson();
