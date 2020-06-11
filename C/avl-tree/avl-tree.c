cbb1ef52f940a048478f14384fd89848392587dc
bashonregardless <theunforgivensol@gmail.com>
2020-06-12 01:43:08 +0530

    chore(avl): ongoing - convert str to array of integers

--- C/avl-tree/avl-tree.c
+++ C/avl-tree/avl-tree.c
@@ -26,53 +26,86 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct *node {
-  int h, key;
-  struct *node lc, rc;
-  int lh = -1;
-  int rh = -1;
-} nd;
+#define arrLen(arr) (sizeof(arr) == 0 ? 0 : sizeof(arr) / sizeof((arr)[0]))
+#define COUNT 10
 
-struct *nd newNode(int key);
-struct *nd bstInsert(char[] *node);
-int height checkHeightInvar(struct *nd node);
+struct avlNode{
+  int h, key, height, lNullHeight, rNullHeight;
+  struct avlNode *lc;
+  struct avlNode *rc;
+};
 
-struct *nd rightRotate(struct *nd node);
-struct *nd leftRotate(struct *nd node);
-struct *nd findSuccessor(struct *nd node);
-struct *nd leftChildFixHeightInvar(struct *nd node);
-struct *nd rightChildFixHeightInvar(struct *nd node);
-void updateHeight(struct *nd node);
+struct avlNode *nodealloc(int key);
+struct avlNode *bstInsert(struct avlNode *node, int key);
+int checkHeightInvar(struct avlNode *node);
 
-int main (int argc, char[] *argv)
+struct avlNode *rightRotate(struct avlNode *node);
+struct avlNode *leftRotate(struct avlNode *node);
+struct avlNode *findSuccessor(struct avlNode *node);
+struct avlNode *leftChildFixHeightInvar(struct avlNode *node);
+struct avlNode *rightChildFixHeightInvar(struct avlNode *node);
+void updateHeight(struct avlNode *node);
+void print2D(struct avlNode *node, int space);
+
+int main (int argc, char *argv[])
 {
-  char[] *inputArr = scanf(&a);
+  int *inputArr[10];
+  int substrl = 0;
 
-  struct *nd root = newNode(1);
+  //printf("%d", arrLen(argv[1]));
+  for(int i = 0; i < arrLen(argv[1]); i++)
+  {
+	if (argv[1][i] != ' ')
+	{
+	  substrl++; 
+	  continue;
+	}
+	else 
+	{
+	  char substr = (char *)calloc(1, substrl);
+	  //inputArr[i] = atoi(strndup(argv[1], i));
+	  substrl = 0;
+	}
+	//printf("%c", argv[1][i]);
+  }
 
-  if (*inputArr.length > 1)
+  //inputArr = argv[1];
+ // for(int i = 0; i < arrLen(inputArr); i++)
+ // {
+ //   printf("%d", *inputArr[i]);
+ // }
+
+  struct avlNode* root = nodealloc(1);
+
+  if (arrLen(inputArr) > 1)
   {
 
   } 
   else {
-	root = bstInsert(*inputArr[0]);
+	root = bstInsert(root, 1);
   }
+
+  print2D(root, COUNT);
 }
 
-struct *nd newNode(int key)
+struct avlNode *nodealloc(int key)
 {
-  struct *nd node = malloc(sizeof(struct *nd));
-  node.key = key;
-  return node;
+  struct avlNode *newNode = (struct avlNode *)malloc(sizeof(struct avlNode));
+  newNode->key = key;
+  newNode->height = 0;
+  newNode->lNullHeight = -1;
+  newNode->rNullHeight = -1;
+  return newNode;
 }
 
-int checkHeightInvar(struct *nd node)
+int checkHeightInvar(struct avlNode *node)
 {
-  return abs(node.lc.height - node.rc.height);
+  return abs(node->lc->height - node->rc->height);
 }
 
-struct *nd leftChildFixHeightInvar(struct *nd node)
+struct avlNode *leftChildFixHeightInvar(struct avlNode *node)
 {
   int xHeightDiff = checkHeightInvar(node);
 
@@ -82,9 +115,9 @@ struct *nd leftChildFixHeightInvar(struct *nd node)
 	// node y is right heavy. Zig-Zag chain
 	if (yHeightDiff == -1) 
 	{
-	  leftRotate(node.lc);
+	  leftRotate(node->lc);
 	  rightRotate(node);
-	  return node.rc;
+	  return node->rc;
 	}
 	// y is left heavy. Straight chain
 	else
@@ -95,7 +128,7 @@ struct *nd leftChildFixHeightInvar(struct *nd node)
   }
 }
 
-struct *nd rightChildFixHeightInvar(struct *nd node) {
+struct avlNode *rightChildFixHeightInvar(struct avlNode *node) {
   int xHeightDiff = checkHeightInvar(node);
 
   if (xHeightDiff > 1) {
@@ -104,9 +137,9 @@ struct *nd rightChildFixHeightInvar(struct *nd node) {
 	// node y is left heavy. Zig-Zag chain
 	if (yHeightDiff == 1) 
 	{
-	  rightRotate(node.rc);
+	  rightRotate(node->rc);
 	  leftRotate(node);
-	  return node.rc;
+	  return node->rc;
 	}
 	// y is right heavy. Straight chain
 	else
@@ -117,84 +150,103 @@ struct *nd rightChildFixHeightInvar(struct *nd node) {
   }
 }
 
-struct *nd bstInsert(struct *nd node, int key)
+struct avlNode *bstInsert(struct avlNode *node, int key)
 {
   // node being inserted as left child
-  if (node.key < stub.key)
+  if (key < node->key)
   {
-	if (node.lc == NULL) {
-	  struct *nd newNode = newNode(key);
-	  node.lc = newNode;
+	if (node->lc == NULL) {
+	  struct avlNode *newNode = nodealloc(key);
+	  node->lc = newNode;
 	  return leftChildFixHeightInvar(node);
 	}
 
-	struct *nd updatedRoot = bstInsert(node.lc);
+	struct avlNode *updatedRoot = bstInsert(node->lc, key);
 
 	// update root after insertion
-	node.lc = updatedRoot;
+	node->lc = updatedRoot;
 	return leftChildFixHeightInvar(node);
   }
 
   // node being inserted as right child
   else
   {
-	if (node.rc == NULL)
+	if (node->rc == NULL)
 	{
-	  struct *nd newNode = newNode(key);
-	  node.rc = newNode;
+	  struct avlNode *newNode = nodealloc(key);
+	  node->rc = newNode;
 	  return rightChildFixHeightInvar(node);
 	}
 
-	struct *nd updatedRoot = bstInsert(node.rc);
+	struct avlNode *updatedRoot = bstInsert(node->rc, key);
 
 	// update root after insertion
-	node.rc = updatedRoot;
+	node->rc = updatedRoot;
 	return rightChildFixHeightInvar(node);
   }
 }
 
-struct *nd findSuccessor(sruct *nd node)
+struct avlNode *findSuccessor(struct avlNode *node)
 {
-  struct *nd stub = node;
+  struct avlNode *stub = node;
 
-  while(stub.left !== NULL)
+  while(stub->lc != NULL)
   {
-	stub = stub.left;
+	stub = stub->lc;
   }
 
   return stub;
 }
 
-struct *nd leftRotate(struct *nd node)
+struct avlNode *leftRotate(struct avlNode *node)
 {
   // store ref to y
-  struct *nd y = node.lc;
-  if (y.rc != NULL) {
-	node.lc = y.rc;
-	y.rc.lc = y;
+  struct avlNode *y = node->lc;
+  if (y->rc != NULL) {
+	node->lc = y->rc;
+	y->rc->lc = y;
 	// make y's rc's lc, rc of y
-	if (y.rc.lc != NULL) {
-	  y.rc = y.rc.lc;
+	if (y->rc->lc != NULL) {
+	  y->rc = y->rc->lc;
 	}
   }
 
   updateHeight(y);
 }
 
-struct *nd rightRotate(struct *nd node)
+struct avlNode *rightRotate(struct avlNode *node)
 {
   // store ref to y
-  struct *nd y = node.lc;
+  struct avlNode *y = node->lc;
 
-  if (y.rc != NULL) {
-	node.lc = y.rc;
+  if (y->rc != NULL) {
+	node->lc = y->rc;
   }
-  y.rc = node;
+  y->rc = node;
 
   updateHeight(node);
 }
 
-void updateHeight(struct *nd node)
+void updateHeight(struct avlNode *node)
 {
-  node.height = 1 + (node.lc.height > node.rc.height ? node.lc.height : node.rc.height);
+  node->height = 1 + (node->lc->height > node->rc->height ? node->lc->height : node->rc->height);
 }
+
+void print2D(struct avlNode *node, int space) {
+  if (node == NULL) {
+    return;
+  }
+
+  space += COUNT;
+
+  print2D(node->rc, space);
+
+  printf("\n");
+  for (int i = COUNT; i < space; i++) {
+    printf(" ");
+  } 
+  printf("%d\n", node->key);
+
+  print2D(node->lc, space);
+}
+
