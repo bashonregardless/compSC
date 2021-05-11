7e28d67de482aa1c88b868b9de008142c5126efd
bashonregardless <theunforgivensol@gmail.com>
2021-05-11 22:39:11 +0530

    chore(C/strtok): working code

--- C-MIT/Stdlib-Implementation/strtok/strtok_impl.c
+++ C-MIT/Stdlib-Implementation/strtok/strtok_impl.c
@@ -1,12 +1,12 @@
 /* string is tokenized at any delimiter in string delims */
-/*
- * Position value of delim character in string " \t\r\n"
- * are enumerated to get integer identifying delimiter.
- */
 #include <stdio.h>
 #include <string.h>
 #define STR_LEN 256
 
+/*
+ * Position value of delim character in string " \t\r\n"
+ * are enumerated to get integer identifying delimiter.
+ */
 unsigned int
 strspn_(const char ch, const char* delims) 
 {
@@ -18,17 +18,16 @@ strspn_(const char ch, const char* delims)
   return -1;
 }
 
-unsigned int 
-strcspn_(const char ch, const char *delims) 
-{
-  unsigned int i;
-
-  for (i = 0; ch != '\0' && strchr(delims, ch); i++) {
-    return i;
-  }
-  return -1;
-}
-
+/* (REFER K&R Pg99)
+ * When an array name is passed to a function, what is passed
+ * is the location of the initial element.
+ * Within the called function, this argument is a local
+ * variabe, and so an array name paramater is a pointer,
+ * that is, a variable containing an address.
+ *
+ * Pointer arithmetic expression like `s++` in callee has no 
+ * effect on character string in the function that called the callee.
+ */
 void
 strtok_impl(char* text, const char* delims, char *token) 
 {
@@ -40,7 +39,6 @@ strtok_impl(char* text, const char* delims, char *token)
   if (text != NULL) {
 	str = text;
 	pnexttoken = str;
-	token = text;
   }
   else {
 	/* look for next delim char in str */
@@ -50,10 +48,11 @@ strtok_impl(char* text, const char* delims, char *token)
 	  if ((delimid = strspn_(*pnexttoken, delims)) == -1) {
 		token[i++] = *pnexttoken;
 		pnexttoken++;
-		continue;
 	  }
-	  else
+	  else {
+		pnexttoken++;
 		break;
+	  }
 	}
 	/* insert null-terminator at  end */
 	token[i] = '\0';
@@ -62,18 +61,17 @@ strtok_impl(char* text, const char* delims, char *token)
 
 int main() {
   char *str = "I am a string.";
-  char token1[STR_LEN];
+  char *token1;
   char token2[STR_LEN];
   char token3[STR_LEN];
   char token4[STR_LEN];
   char token5[STR_LEN];
 
-  strtok_impl(str, " \t\r\n", &token1);
+  strtok_impl(str, " \t\r\n", token1);
   strtok_impl(NULL, " \t\r\n", token2);
   strtok_impl(NULL, " \t\r\n", token3);
   strtok_impl(NULL, " \t\r\n", token4);
   strtok_impl(NULL, " \t\r\n", token5);
-  printf("token1: %s\n", token1);
   printf("token2: %s\n", token2);
   printf("token3: %s\n", token3);
   printf("token4: %s\n", token4);
