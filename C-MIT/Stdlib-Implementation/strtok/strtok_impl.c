8532c9a047ff3c80ff97ded93f7b28a57d96d3f0
bashonregardless <theunforgivensol@gmail.com>
2021-05-11 14:08:00 +0530

    chore(C/strtok)

--- C-MIT/Stdlib-Implementation/strtok/strtok_impl.c
+++ C-MIT/Stdlib-Implementation/strtok/strtok_impl.c
@@ -1,8 +1,15 @@
 /* string is tokenized at any delimiter in string delims */
+/*
+ * Position value of delim character in string " \t\r\n"
+ * are enumerated to get integer identifying delimiter.
+ */
+#include <stdio.h>
 #include <string.h>
 #define STR_LEN 256
 
-unsigned int strspn_(const char ch, const char* delims) {
+unsigned int
+strspn_(const char ch, const char* delims) 
+{
   unsigned int i;
 
   for (i = 0; ch != '\0' && strchr(delims, ch); i++) {
@@ -11,7 +18,9 @@ unsigned int strspn_(const char ch, const char* delims) {
   return -1;
 }
 
-unsigned int strcspn_(const char ch, const char *delims) {
+unsigned int 
+strcspn_(const char ch, const char *delims) 
+{
   unsigned int i;
 
   for (i = 0; ch != '\0' && strchr(delims, ch); i++) {
@@ -20,45 +29,53 @@ unsigned int strcspn_(const char ch, const char *delims) {
   return -1;
 }
 
-char* strtok_impl(char* text, const char* delims, char* token) {
+void
+strtok_impl(char* text, const char* delims, char *token) 
+{
   static const char *str;
-  static char pnexttoken;
-  char curtoken = pnexttoken;
-  int i;
+  static char *pnexttoken;
+  int i, delimid;
 
   /* initialize */
-  if (strcmp(text, "") == 0) {
-    return NULL;
+  if (text != NULL) {
+	str = text;
+	pnexttoken = str;
+	token = text;
   }
-
-  str = text;
-  if (text == NULL) {
+  else {
 	/* look for next delim char in str */
-	while (pnexttoken != '\0') {
-	  if ((strspn_(pnexttoken, delims)) == -1) {
+	/* copy new token from str to token[] */
+	i = 0;
+	while (1) {
+	  if ((delimid = strspn_(*pnexttoken, delims)) == -1) {
+		token[i++] = *pnexttoken;
 		pnexttoken++;
 		continue;
 	  }
-	}
-	/* copy new token from str to token[] */
-	for (i = 0; curtoken != pnexttoken; i++) {
-	  token[i] = curtoken;
+	  else
+		break;
 	}
 	/* insert null-terminator at  end */
-	(token[i] = '\0');
+	token[i] = '\0';
   }
-
-  /* find start of token in text */
-  if (*text == '\0') {
-    return NULL;
-  }
-
-  return token;
 }
 
 int main() {
   char *str = "I am a string.";
-  char token[STR_LEN];
+  char token1[STR_LEN];
+  char token2[STR_LEN];
+  char token3[STR_LEN];
+  char token4[STR_LEN];
+  char token5[STR_LEN];
 
-  strtok_impl(str, " \t\r\n", token);
+  strtok_impl(str, " \t\r\n", &token1);
+  strtok_impl(NULL, " \t\r\n", token2);
+  strtok_impl(NULL, " \t\r\n", token3);
+  strtok_impl(NULL, " \t\r\n", token4);
+  strtok_impl(NULL, " \t\r\n", token5);
+  printf("token1: %s\n", token1);
+  printf("token2: %s\n", token2);
+  printf("token3: %s\n", token3);
+  printf("token4: %s\n", token4);
+  printf("token5: %s\n", token5);
 }
