94ccc13ce410cd24b76963e89df81a78ba3dc5f5
bashonregardless <theunforgivensol@gmail.com>
2021-05-15 11:21:09 +0530

    chore(cleanup and minor code changes)

--- JS/pureJS/Product-Table/js/partial.js
+++
@@ -1,7 +0,0 @@
-var PARTIAL = function partial(func, ...args) {
-  var args = Array.prototype.slice.call(arguments, 1);
-  return function() {
-    var allArguments = args.concat(Array.prototype.slice.call(arguments));
-    return func.apply(this, allArguments);
-  }
-} 
