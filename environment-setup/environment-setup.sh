af1ed3e820f9c8a81d8b71065671cd60320665dc
bashonregardless <harshvardhan0622@gmail.com>
2022-07-04 16:39:30 +0530

    update: env setup script updated to keep cd and clone of minpac outside subshell as minpac installation not showing up

--- environment-setup/environment-setup.sh
+++ environment-setup/environment-setup.sh
@@ -233,9 +233,7 @@ fi
 # add minpac vim plugin manager (See Modern vim craft)
 [ ! -d "$HOME/.config/nvim/pack/minpac/opt" ] && mkdir -p "$HOME/.config/nvim/pack/minpac/opt"
 cd "$HOME/.config/nvim/pack/minpac/opt"
-(
 # TODO minpac does not show up with the code run in subshell
-)
 git clone https://github.com/k-takata/minpac.git
 cd -
 
