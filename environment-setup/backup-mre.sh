c04d80789cf9ac58e7a330eca687ab0c0c806931
Ubuntu <bashonregardless>
2022-07-11 20:52:46 +0530

    update: how-to updated with 'Communicate a value back from a function to the main body of the script.'

--- environment-setup/backup-mre.sh
+++ environment-setup/backup-mre.sh
@@ -9,13 +9,13 @@ create_array() {
   passed_arg="$1"
   passed_array=$(cat "$passed_arg")
 
-  echo "Inside function passed_array length is ${#passed_array[@]} and its contents are : ${passed_array[@]}"
+  # echo "Inside function passed_array length is ${#passed_array[@]} and its contents are : ${passed_array[@]}"
 
   echo "${passed_array[@]}"
 }
 
 IFS=$'\n'
-array=create_array "$1"
-# array=($(create_array $1))
+# array=create_array "$1"
+array=($(create_array $1))
 
 echo "Returned array length is ${#array[@]} and its contents are : ${array[@]}"
