9a20c5f3b7681fe727f8cf8170d8c960b302bea1
Ubuntu <bashonregardless>
2022-07-11 17:12:20 +0530

    update: env setup backup mre - after reading https://www.linuxtopia.org/online_books/advanced_bash_scripting_guide/assortedtips.html, added IFS to main body of script

--- environment-setup/backup-mre.sh
+++ environment-setup/backup-mre.sh
@@ -9,12 +9,13 @@ create_array() {
   passed_arg="$1"
   passed_array=$(cat "$passed_arg")
 
-  # echo "Inside function passed_array length is ${#passed_array[@]} and its contents are : ${passed_array[@]}"
+  echo "Inside function passed_array length is ${#passed_array[@]} and its contents are : ${passed_array[@]}"
 
   echo "${passed_array[@]}"
 }
 
-# create_array "$2"
-array=($(create_array $1))
+IFS=$'\n'
+array=create_array "$1"
+# array=($(create_array $1))
 
 echo "Returned array length is ${#array[@]} and its contents are : ${array[@]}"
